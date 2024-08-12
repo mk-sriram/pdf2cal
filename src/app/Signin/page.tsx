e3581fc192d678b78de6ea6bf86c5170d40d4a0d
Sriram MK <mksriram@umich.edu>
2024-08-13 01:51:03 +0300

    redirect URL

--- src/app/Signin/page.tsx
+++ src/app/Signin/page.tsx
@@ -16,7 +16,7 @@ const page = () => {
           access_type: "offline",
           prompt: "consent",
         },
-        redirectTo: `${location.origin}/api/auth/callback`,
+        redirectTo: `https://pdf2cal.vercel.app/api/auth/callback`,
       },
     });
   };
