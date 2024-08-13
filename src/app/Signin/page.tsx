02404021dbbeca5dae7334ee2bc5951ce5bb3648
Sriram MK <mksriram@umich.edu>
2024-08-13 05:17:00 +0300

    update

--- src/app/Signin/page.tsx
+++ src/app/Signin/page.tsx
@@ -2,7 +2,6 @@
 import { useRouter } from "next/navigation";
 import { createClient } from "@/utils/supabase/client";
 const page = () => {
-  const router = useRouter();
   const supabase = createClient();
 
   const handleSignIn = async () => {
@@ -28,110 +27,6 @@ const page = () => {
             Sign in
           </h2>
 
-          {/*           
-          <p className="text-gray-500 text-sm mt-[10px] text-center">
-            Not a member?{" "}
-            <Link
-              href="/Signup"
-              className="text-[#0b7dffd4] hover:underline ml-1 whitespace-nowrap font-semibold"
-            >
-              {" "}
-              Sign up
-            </Link>
-          </p>
-          <form className="mt-10 space-y-4">
-            <div>
-              <label className="text-gray-800 text-sm mb-2 block">email</label>
-              <div className="relative flex items-center">
-                <input
-                  name="email"
-                  type="text"
-                  required
-                  className="w-full text-gray-800 text-sm border border-gray-300 px-4 py-3 rounded-md outline-blue-600"
-                  placeholder="Enter email"
-                />
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="#bbb"
-                  stroke="#bbb"
-                  className="w-4 h-4 absolute right-4"
-                  viewBox="0 0 24 24"
-                >
-                  <circle cx="10" cy="7" r="6" data-original="#000000"></circle>
-                  <path
-                    d="M14 15H6a5 5 0 0 0-5 5 3 3 0 0 0 3 3h12a3 3 0 0 0 3-3 5 5 0 0 0-5-5zm8-4h-2.59l.3-.29a1 1 0 0 0-1.42-1.42l-2 2a1 1 0 0 0 0 1.42l2 2a1 1 0 0 0 1.42 0 1 1 0 0 0 0-1.42l-.3-.29H22a1 1 0 0 0 0-2z"
-                    data-original="#000000"
-                  ></path>
-                </svg>
-              </div>
-            </div>
-
-            <div>
-              <label className="text-gray-800 text-sm mb-2 block">
-                Password
-              </label>
-              <div className="relative flex items-center">
-                <input
-                  name="password"
-                  type="password"
-                  required
-                  className="w-full text-gray-800 text-sm border border-gray-300 px-4 py-3 rounded-md outline-blue-600"
-                  placeholder="Enter password"
-                />
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="#bbb"
-                  stroke="#bbb"
-                  className="w-4 h-4 absolute right-4 cursor-pointer"
-                  viewBox="0 0 128 128"
-                >
-                  <path
-                    d="M64 104C22.127 104 1.367 67.496.504 65.943a4 4 0 0 1 0-3.887C1.367 60.504 22.127 24 64 24s62.633 36.504 63.496 38.057a4 4 0 0 1 0 3.887C126.633 67.496 105.873 104 64 104zM8.707 63.994C13.465 71.205 32.146 96 64 96c31.955 0 50.553-24.775 55.293-31.994C114.535 56.795 95.854 32 64 32 32.045 32 13.447 56.775 8.707 63.994zM64 88c-13.234 0-24-10.766-24-24s10.766-24 24-24 24 10.766 24 24-10.766 24-24 24zm0-40c-8.822 0-16 7.178-16 16s7.178 16 16 16 16-7.178 16-16-7.178-16-16-16z"
-                    data-original="#000000"
-                  ></path>
-                </svg>
-              </div>
-            </div>
-
-            <div className="flex flex-wrap items-center justify-between gap-4">
-              <div className="flex items-center">
-                <input
-                  id="remember-me"
-                  name="remember-me"
-                  type="checkbox"
-                  className="h-4 w-4 shrink-0 text-blue-600 focus:ring-blue-500 border-gray-300 rounded"
-                />
-                <label className="ml-3 block text-sm text-gray-800">
-                  Remember me
-                </label>
-              </div>
-              <div className="text-sm">
-                <a
-                  href="jajvascript:void(0);"
-                  className="text-[#0b7dffd4] hover:underline font-semibold"
-                >
-                  Forgot password?
-                </a>
-              </div>
-            </div>
-
-            <div className="!mt-8">
-              <button
-                formAction={login}
-                type="submit"
-                className="w-full py-3 px-4 text-sm tracking-wide rounded-lg text-white bg-[#0b7dffd4] hover:bg-[#6dc1fc] focus:outline-none"
-              >
-                Sign in
-              </button>
-            </div>
-          </form>
-          <div className="flex items-center justify-center mt-8">
-            <div className="flex-grow border-t border-gray-300"></div>
-            <span className="mx-4 text-gray-800 text-sm flex-shrink-0">
-              Or continue with
-            </span>
-            <div className="flex-grow border-t border-gray-300"></div>
-          </div> */}
 
           <div className="space-x-6 flex justify-center mt-8 ">
             <button
