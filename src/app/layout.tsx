f68b726eff8d7c4dbd24a9e5912b0eb4512af5a6
Sriram MK <mksriram@umich.edu>
2024-08-15 22:57:51 +0300

    analytics vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/Navbar";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
@@ -13,14 +13,13 @@ export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
-}>) { 
-
-  
+}>) {
   return (
     <html lang="en" data-theme="cmyk">
       <body className={inter.className}>
         {/* //<Navbar /> */}
         {children}
+        <Analytics />
       </body>
     </html>
   );
