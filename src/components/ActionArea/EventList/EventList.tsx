cd005883237f2904729ec7804e08c392c0669730
Sriram MK <mksriram@umich.edu>
2024-08-17 09:05:05 +0300

    added private policy link

--- src/components/ActionArea/EventList/EventList.tsx
+++ src/components/ActionArea/EventList/EventList.tsx
@@ -23,8 +23,11 @@ interface EventListProps {
   onDeleteTask: (index: number) => void;
 }
 
-const EventList: React.FC<EventListProps> = ({ jsonData, loading, onDeleteTask}) => {
-  
+const EventList: React.FC<EventListProps> = ({
+  jsonData,
+  loading,
+  onDeleteTask,
+}) => {
   return (
     <div className="flex xl:w-[38rem] lg:w-[35rem] md:w-[30rem]  sm:w-[25rem] h-[95%] p-4 justify-center shadow-xl rounded-2xl bg-gray-100">
       <div className="overflow-scroll w-full ">
@@ -34,10 +37,7 @@ const EventList: React.FC<EventListProps> = ({ jsonData, loading, onDeleteTask})
             <div className="animate-pulse w-full ">
               {/* Add the number of skeleton loaders equal to the number of event cards you expect */}
               {[...Array(4)].map((_, index) => (
-                <div
-                  key={index}
-                  className="p-4 mb-4 bg-gray-300 rounded-lg "
-                >
+                <div key={index} className="p-4 mb-4 bg-gray-300 rounded-lg ">
                   <div className="h-6 mb-2 bg-gray-400 rounded w-1/2 "></div>
                   <div className="h-6 mb-2 bg-gray-400 rounded w-1/4"></div>
                   <div className="h-6 mb-2 bg-gray-400 rounded w-3/4"></div>
