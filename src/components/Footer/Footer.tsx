7e27cf442edc535c03e13bbfbb3404f47371c89b
Sriram MK <mksriram@umich.edu>
2024-08-17 09:33:49 +0300

    Update Footer.tsx

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -31,7 +31,7 @@ const Footer = () => {
             </li>
             <li>
               <Link
-                href="https://www.pdf2calendar.com/private-policy/"
+                href="https://www.pdf2calendar.com/private-policy"
                 className="transition-all transform active:scale-[0.98] hover:scale-[1.01] mr-4"
               >
                 Privacy Policy
