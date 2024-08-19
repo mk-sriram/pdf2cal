b0c87800229abf54a1c2e058b798eb7b548c42f5
Sriram MK <mksriram@umich.edu>
2024-08-19 20:10:31 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,11 @@ import { Analytics } from "@vercel/analytics/react";
 import Footer from "@/components/Footer/Footer";
 const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
-  title: "Pdf2Cal",
-  description: "Schedule to Calendar made Easy",
+  title: "Convert Schedules to Calendar Events and Tasks | Pdf2Calendar",
+  description:
+    "Effortlessly convert your Schedules to calendar events in seconds. Save time and stay organized with Pdf2Calendar",
+  keywords:
+    "PDF to calendar, convert PDF to calendar, calendar events, Google Calendar, Apple Calendar, convert images to calendar events, DOCX to calendar, JPG to calendar, PNG to calendar, PDF to Google Calendar, PDF to Google Tasks, image to Google Calendar, DOCX to Google Calendar, convert schedules to calendar, automate calendar events, convert PDF to tasks, PDF to Google Tasks",
   other: {
     "google-site-verification": "BP7SP3zkjzjVJYq-pxdAftgQpbUN0Mksv_0aaIiZhF0",
   },
