dce4921a716609114cdca909cfdaff2a8bb21f7b
Sriram MK <mksriram@umich.edu>
2024-08-19 22:31:29 +0300

    increased max dureaction

--- src/app/api/get-calendar-list/route.ts
+++ src/app/api/get-calendar-list/route.ts
@@ -1,4 +1,4 @@
-export const maxDuration = 15;
+export const maxDuration = 50;
 export const dynamic = "force-dynamic";
 
 import { NextRequest, NextResponse } from "next/server";
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,6 @@ const page = () => {
         <ActionArea />
       </div>
       <FaqSection />
-     
     </>
   );
 };
